890dd0eab6a54b0e7d2f99646a8ce4dadba13a94
bombeyMix <myrooom911@gmail.com>
2018-11-09 20:34:58 +0200

    Load Image

--- src/main/java/ua/logos/controller/ProductController.java
+++ src/main/java/ua/logos/controller/ProductController.java
@@ -3,23 +3,33 @@ package ua.logos.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ua.logos.domain.ErrorDTO;
 import ua.logos.domain.ProductDTO;
+import ua.logos.service.FileStorageService;
 import ua.logos.service.ProductService;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("products")
 public class ProductController {
     @Autowired
-    ProductService productService;
+    private ProductService productService;
+    @Autowired
+    private FileStorageService fileStorageService;
 
     //localhost:8080/products
 
@@ -65,4 +75,31 @@ public class ProductController {
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
+    @PostMapping("{productId}/image")
+    public ResponseEntity<?> uploadImage(@PathVariable("productId") Long id, @RequestParam("file") MultipartFile file) {
+        System.out.println(file.getOriginalFilename());
+        fileStorageService.storeFile(file);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("image/{fileName}")
+    public ResponseEntity<?> getImage(@PathVariable ("fileName") String fileName, HttpServletRequest request){
+        System.out.println(fileName);
+        Resource resource = fileStorageService.loadFile(fileName);
+        String contentType = null;
+        try {
+            contentType=request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        if (contentType==null){
+            contentType="application/octet-stream";
+        }
+        return  ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline: fileName=\"" +resource.getFilename()+"\"")
+                .body(resource);
+    }
 }
